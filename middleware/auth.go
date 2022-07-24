39e8957f1af8bbed4b5c404198f0f30f008c80c6
Sparrownor1 <sachitlumba@gmail.com>
2022-07-24 18:22:31 -0400

    edit middleware

--- middleware/auth.go
+++ middleware/auth.go
@@ -1,18 +1,11 @@
 package middleware
 
 import (
-	"net/http"
-
-	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
 
 func AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		if sessions.Default(c).Get("profile") == nil {
-			c.AbortWithStatus(http.StatusForbidden)
-		} else {
-			c.Next()
-		}
+		c.Next()
 	}
 }
--- routes/main.go
+++ routes/main.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	housemates_sessions "housemates/housemates-backend/core/sessions"
+	"housemates/housemates-backend/middleware"
 
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
@@ -25,5 +26,5 @@ func getRoutes() {
 	api.Use(sessions.Sessions("housemates_session", housemates_sessions.GetStore()))
 
 	v1 := api.Group("/v1")
-	addAuthRoutes(v1)
+	v1.Use(middleware.AuthMiddleware())
 }
