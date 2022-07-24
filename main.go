239d0202ee661959a1f0a5f0585650ed10e71e61
Sparrownor1 <sachitlumba@gmail.com>
2022-07-23 22:31:13 -0400

    init sessions

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"housemates/housemates-backend/core/authenticator"
 	"housemates/housemates-backend/core/db"
+	"housemates/housemates-backend/core/sessions"
 	"housemates/housemates-backend/routes"
 )
 
 func main() {
 	db.Init()
+	sessions.Init()
 	authenticator.Init()
 	routes.Run()
 }
