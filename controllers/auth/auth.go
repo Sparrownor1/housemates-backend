f2c1cbc42efdaf3ab9c8dd05f0e0cac4d4a5c738
Sparrownor1 <sachitlumba@gmail.com>
2022-07-23 22:11:46 -0400

    add login endpoint

--- controllers/auth/auth.go
+++ controllers/auth/auth.go
@@ -1,13 +1,45 @@
 package auth
 
 import (
+	"crypto/rand"
+	"encoding/base64"
+	"housemates/housemates-backend/core/authenticator"
 	"net/http"
 
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
 
 func Login(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "Login Stub")
+	auth := authenticator.GetAuthenticator()
+
+	state, err := generateRandomState()
+	if err != nil {
+		c.String(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	// Save the state inside the session.
+	session := sessions.Default(c)
+	session.Set("state", state)
+	if err := session.Save(); err != nil {
+		c.String(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.Redirect(http.StatusTemporaryRedirect, auth.AuthCodeURL(state))
+}
+
+func generateRandomState() (string, error) {
+	b := make([]byte, 32)
+	_, err := rand.Read(b)
+	if err != nil {
+		return "", err
+	}
+
+	state := base64.StdEncoding.EncodeToString(b)
+
+	return state, nil
 }
 
 func Register(c *gin.Context) {
--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"housemates/housemates-backend/core/authenticator"
 	"housemates/housemates-backend/core/db"
 	"housemates/housemates-backend/routes"
 )
 
 func main() {
 	db.Init()
+	authenticator.Init()
 	routes.Run()
 }
