aabfeef53dabccbad18d5a971d39dd295cdf4545
Sparrownor1 <sachitlumba@gmail.com>
2022-07-23 21:49:21 -0400

    add sessions to router

--- routes/main.go
+++ routes/main.go
@@ -1,8 +1,10 @@
 package routes
 
 import (
+	housemates_sessions "housemates/housemates-backend/core/sessions"
 	"housemates/housemates-backend/middleware"
 
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
 
@@ -18,5 +20,8 @@ func Run() {
 
 // getRoutes will create our routes of our entire application
 func getRoutes() {
+	// sessions
+	router.Use(sessions.Sessions("housemates_session", housemates_sessions.GetStore()))
+	// auth middleware
 	router.Use(middleware.AuthMiddleware())
 }
