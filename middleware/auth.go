bb6236f724c11dfe061eb778ed19af8ea0b29147
Sparrownor1 <sachitlumba@gmail.com>
2022-07-24 20:28:30 -0400

    add authenticator and middleware

--- middleware/auth.go
+++ middleware/auth.go
@@ -2,36 +2,25 @@ package middleware
 
 import (
 	"fmt"
-	"log"
-	"os"
-	"time"
+	"housemates/housemates-backend/core/authenticator"
+	"net/http"
 
-	"github.com/MicahParks/keyfunc"
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v4"
-	"github.com/joho/godotenv"
 )
 
 func AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		_, err := validateToken(c.Request.Header["Bearer"][0])
+		if err != nil {
+			c.AbortWithStatus(http.StatusUnauthorized)
+		}
 		c.Next()
 	}
 }
 
 func validateToken(tokenString string) (interface{}, error) {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	auth0Domain := os.Getenv("AUTH0_DOMAIN")
-	jwksUrl := fmt.Sprintf("https://%s/.well-known/jwks.json", auth0Domain)
-	jwks, err := keyfunc.Get(jwksUrl, keyfunc.Options{
-		RefreshRateLimit: time.Second * 12,
-	})
-	if err != nil {
-		log.Fatalf("Failed to get the JWKS from the given URL.\nError: %s", err)
-	}
+	jwks := authenticator.GetJWKS()
 
 	token, err := jwt.Parse(tokenString, jwks.Keyfunc)
 	if err != nil {
