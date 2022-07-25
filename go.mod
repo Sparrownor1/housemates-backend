99c7facd8eeeac8fa0ed502716dbaf8124b7b741
Sparrownor1 <sachitlumba@gmail.com>
2022-07-24 20:17:36 -0400

    i think i have something functional

--- go.mod
+++ go.mod
@@ -10,11 +10,14 @@ require (
 )
 
 require (
+	github.com/MicahParks/keyfunc v1.2.0 // indirect
+	github.com/auth0/go-jwt-middleware/v2 v2.0.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.0 // indirect
 	github.com/goccy/go-json v0.9.10 // indirect
+	github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-cmp v0.5.8 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
--- middleware/auth.go
+++ middleware/auth.go
@@ -1,11 +1,42 @@
 package middleware
 
 import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/MicahParks/keyfunc"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/joho/godotenv"
 )
 
 func AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Next()
 	}
+}
+
+func validateToken(tokenString string) (interface{}, error) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	auth0Domain := os.Getenv("AUTH0_DOMAIN")
+	jwksUrl := fmt.Sprintf("https://%s/.well-known/jwks.json", auth0Domain)
+	jwks, err := keyfunc.Get(jwksUrl, keyfunc.Options{
+		RefreshRateLimit: time.Second * 12,
+	})
+	if err != nil {
+		log.Fatalf("Failed to get the JWKS from the given URL.\nError: %s", err)
+	}
+
+	token, err := jwt.Parse(tokenString, jwks.Keyfunc)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse token: %w", err)
+	}
+
+	return token, nil
 }
